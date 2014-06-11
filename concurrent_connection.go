74eb2f447ddc5fdb1a1828d446dde3c751a7b10e
Ben Cox <benjamin.cox@jadedpixel.com>
2014-06-11 11:25:16 -0400

    Command line input

--- concurrent_connection.go
+++ concurrent_connection.go
@@ -6,6 +6,7 @@ import (
   "os"
   )
 
+var completed int = 0
 
 func do_connect(url string, n int){
   for i := 0; i < n; i++ {
@@ -20,23 +21,40 @@ func do_connect(url string, n int){
         fmt.Println(err)
         os.Exit(1)
       }
-      if status == "200 OK" {
+      if status != "200 OK" {
         fmt.Println(status)
       }
     }
   }
-  fmt.Println("Completed.")
+
+  completed = completed + 1
+
+  fmt.Println("Completed Routine: ", completed)
+  if completed == 10 {
+    fmt.Println("ALL DONE :D")
+  }
 }
 
 func main() {
 
   test_url := "http://benify.myshopify.com/collections/all/products.json?limit=250"
-  number := 100
+
+  var url string
+  fmt.Println("Enter URL:")
+  fmt.Scanln(&url)
+  if url == ""{
+    url = test_url
+  }
+
+  var number int
+  fmt.Println("Enter connections per routine (ten routines): ")
+  fmt.Scanln(&number)
 
   for i := 0; i < 10; i++ {
     go do_connect(test_url, number)
   }
   var input string
   fmt.Scanln(&input)
+  fmt.Println(input)
 }
 
