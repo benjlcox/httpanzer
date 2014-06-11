44fc501f510f91b9dcf99e21fb91b703826c1d4e
Ben Cox <benjamin.cox@jadedpixel.com>
2014-06-11 10:54:43 -0400

    Concurrency ftw

--- concurrent_connection.go
+++ concurrent_connection.go
@@ -6,18 +6,37 @@ import (
   "os"
   )
 
-func main() {
-  response, err := http.Get("http://benify.myshopify.com/collections/all/products.json?limit=250")
-  if err != nil {
-      fmt.Printf("%s", err)
+
+func do_connect(url string, n int){
+  for i := 0; i < n; i++ {
+    response, err := http.Get(url)
+    if err != nil {
+      fmt.Println(err)
       os.Exit(1)
-  } else {
+    } else {
       defer response.Body.Close()
-      contents := response.Status
+      status := string(response.Status)
       if err != nil {
-          fmt.Printf("%s", err)
-          os.Exit(1)
+        fmt.Println(err)
+        os.Exit(1)
       }
-      fmt.Printf("%s\n", string(contents))
+      if status == "200 OK" {
+        fmt.Println(status)
+      }
+    }
   }
+  fmt.Println("Completed.")
 }
+
+func main() {
+
+  test_url := "http://benify.myshopify.com/collections/all/products.json?limit=250"
+  number := 100
+
+  for i := 0; i < 10; i++ {
+    go do_connect(test_url, number)
+  }
+  var input string
+  fmt.Scanln(&input)
+}
+
