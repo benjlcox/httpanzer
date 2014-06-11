c761f54202e14ddf2a6c9e9b98846fb785c78311
Ben Cox <benjamin.cox@jadedpixel.com>
2014-06-11 11:29:19 -0400

    Print number of ok

--- concurrent_connection.go
+++ concurrent_connection.go
@@ -3,26 +3,26 @@ package main
 import (
   "fmt"
   "net/http"
-  "os"
   )
 
 var completed int = 0
+var ok int = 0
 
 func do_connect(url string, n int){
   for i := 0; i < n; i++ {
     response, err := http.Get(url)
     if err != nil {
       fmt.Println(err)
-      os.Exit(1)
     } else {
       defer response.Body.Close()
       status := string(response.Status)
       if err != nil {
         fmt.Println(err)
-        os.Exit(1)
       }
       if status != "200 OK" {
         fmt.Println(status)
+      } else {
+        ok = ok + 1
       }
     }
   }
@@ -32,6 +32,7 @@ func do_connect(url string, n int){
   fmt.Println("Completed Routine: ", completed)
   if completed == 10 {
     fmt.Println("ALL DONE :D")
+    fmt.Println("Number of 200 OK: ", ok)
   }
 }
 
